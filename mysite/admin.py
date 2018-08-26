d3d00b436136f765e7c2196942f662460cac577f
Yt-machine <slcy1231@163.com>
2018-08-26 20:49:29 +0800

    first commit

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Mood, Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'message', 'enabled', 'pub_time')
+    ordering = ('-pub_time',)
+
+
+admin.site.register(Mood)
+admin.site.register(Post, PostAdmin)
--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import get_template
+from .models import Post, Mood
+
 
 # Create your views here.
+def index(request):
+    template = get_template('index.html')
+    posts = Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
+    moods = Mood.objects.all()
+    html = template.render(locals())
+    return HttpResponse(html)
